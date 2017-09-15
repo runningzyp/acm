75f80aa6359e2ac676a28d9a1f78997d45010255
zhanyunpeng <136688059@qq.com>
2017-09-15 21:32:30 +0800

    change

--- config.py
+++ config.py
@@ -22,18 +22,18 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    '''
+
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + \
         os.path.join(basedir, 'data-dev.sqlite')
-    '''
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:zhan@1234@rm-uf6h3xy2ty9zc6cu7o.mysql.rds.aliyuncs.com/acm'
+
+    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:zhan@1234@rm-uf6h3xy2ty9zc6cu7o.mysql.rds.aliyuncs.com/acm'
 
 
 class TestingConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
-    
+
 
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
